9d6b94558b9c3e99701f68fbc932be96b5febba7
Mathanrajseenuvasan <53528764+Mathanrajseenuvasan@users.noreply.github.com>
2019-08-06 21:11:41 +0530

    Add files via upload

--- MyStudentApp.java
+++ MyStudentApp.java
@@ -1,13 +1,12 @@
 package First;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class MyStudentApp {
 	static Scanner getvalue = new Scanner(System.in);
 
-	static List<Student> listOfStudent = new ArrayList<>();
+	static ArrayList<Student> listOfStudent = new ArrayList<>();
 
 	public static void menu() {
 		System.out.println("1.Add \n2.Update \n3.show \n4.Delete");
@@ -25,13 +24,16 @@ public class MyStudentApp {
 		case 3:
 			System.out.println("Show all Student Details");
 			show();
+			menu();
 			break;
+		case 4:
+			delete();
 		default:
 			System.out.println("I AM DEFAULT...!!!");
 		}
 	}
 
-	    static void addStudent() {
+	static void addStudent() {
 		Student student = new Student();
 		System.out.println("Enter student name : ");
 		student.setName(getvalue.next());
@@ -43,40 +45,62 @@ public class MyStudentApp {
 		System.out.println(listOfStudent.size());
 		add();
 	}
-	
-	    static void add() {
+
+	static void add() {
 		System.out.println("Do you want to add another student then pressn yes");
 		String answer = getvalue.next();
-		if(answer.equals("no")) {
+		if (answer.equals("no")) {
 			System.out.println("thanks for adding details");
-		}else {
-		menu();
+			menu();
+		} else {
+			addStudent();
 		}
 	}
 
 	public static void show() {
-		for (Student xxx : listOfStudent) {
-			System.out.println(xxx.toString());
-			System.out.println(xxx.toString());
-			System.out.println(xxx.getName());
-			
+		for (Student stu : listOfStudent) {
+			System.out.println(stu);
+
 		}
 	}
-	
+
 	public static void update() {
 		Student student1 = new Student();
 		System.out.println("Enter the student name you want to make changes");
 		String cname = getvalue.next();
-		if (cname == student1.getName()) {
-			System.out.println("name matched");
+
+		for (int i = 0; i < listOfStudent.size(); i++) {
+
+			if (cname.equals(listOfStudent.get(i).getName())) {
+				System.out.println("name matched");
+				System.out.println(listOfStudent.get(i));
+				System.out.println("Enter name");
+				student1.setName(getvalue.next());
+				System.out.println("Enter age");
+				student1.setAge(getvalue.nextInt());
+				System.out.println("Enter score");
+				student1.setScore(getvalue.nextInt());
+				listOfStudent.set(i, student1);
+				menu();
 			}
-		
-	
+
+		}
 	}
+
 	public static void delete() {
-		
-		
-	}
+		Student student2 = new Student();
+		System.out.println("Enter the student name you want to delete");
+		String sname = getvalue.next();
+
+		for (int i = 0; i < listOfStudent.size(); i++) {
+			if (sname.equals(listOfStudent.get(i).getName())) {
+			listOfStudent.remove(i);
+			menu();
+			
+		}
+			}
+		}
+	
 
 	public static void main(String[] args) {
 		// MyStudentApp student =new MyStudentApp();
